007c79acfa858790c6568de1a4d70fdcf16ff71c
Francis Flanagan <7flanaganf@gmail.com>
2023-11-25 21:54:17 +0900

    sidebar styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,27 +6,27 @@ import user from '../images/user.svg';
 
 export const Sidebar = () => {
   return (
-    <aside className="flex flex-col justify-between bg-sidebar text-white text-xl w-[239px]">
-      <div>
-        <a className="flex">
+    <aside className=" px-[27px] flex flex-col justify-between bg-sidebar text-white text-xl w-[239px] pt-[84px] ">
+      <div className="gap-y-7 flex flex-col">
+        <a className="flex gap-x-2">
           <img src={airplay} />
           Dashboard
         </a>
-        <a className="flex">
+        <a className="flex gap-x-2">
           <img src={clipboard} />
-          "Upcoming visits
+          Upcoming visits
         </a>
-        <a className="flex">
+        <a className="flex gap-x-2">
           <img src={settings} />
-          "Patients
+          Patients
         </a>
-        <a className="flex">
+        <a className="flex gap-x-2">
           <img src={settings} />
           Settings
         </a>
       </div>
       <div>
-        <a className="flex">
+        <a className="flex gap-x-2">
           <img src={user} />
           Profile
         </a>
