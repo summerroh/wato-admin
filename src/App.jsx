e0a7928b18c886ce5f8f9051ad1eee2788021e7a
Emre Ceyhan <a@emreceyhan.com>
2023-11-25 21:33:46 +0900

    feat: api js added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { API } from '../api'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
